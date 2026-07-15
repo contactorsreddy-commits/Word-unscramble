d24f3f21849d849f0b411cba1e47bcd016a35322
naithik <contact.orsreddy@gmail.com>
2026-07-15 10:44:08 -0400

    quiz

---
+++ quiz.js
@@ -0,0 +1,8 @@
+var question= alert("What is the capital of france?\n"+"1. London\n"+"2. Paris\n"+"3. Berlin\n"+"4. Madrid");
+var useranswer= prompt("Enter your answer")
+if(useranswer=="paris"){
+    alert("correct answer paris is the capital of france");
+}
+else{
+    alert("sorry the correct is paris")
+}
