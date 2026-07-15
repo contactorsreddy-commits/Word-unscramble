f97447967c5c23200db34fd576f642256005f775
naithik <contact.orsreddy@gmail.com>
2026-07-15 10:24:58 -0400

    word unscramble

---
+++ word.js
@@ -0,0 +1,9 @@
+var jumbleword="lwrdo";
+var correctword="world";
+var userguess=prompt("unscramble the jumbleword-" + jumbleword);
+if(userguess==correctword) {
+    alert("congrasulations you unscrambled the word correctly");
+}
+else{
+    alert("sorry the correct answer is world");
+}
